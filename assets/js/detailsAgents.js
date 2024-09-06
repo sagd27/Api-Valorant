93077f2222f21c2ec9aa69434bfb67e3bdfd1caa
Juan Bocanegra <jbocanegrae@unal.edu.co>
2024-09-06 12:26:00 -0500

    detailsAgents.js creado

---
+++ assets/js/detailsAgents.js
@@ -0,0 +1,22 @@
+let url = 'https://valorant-api.com/v1/agents';
+
+const {createApp} = Vue
+
+const app = createApp({
+    data() {
+        return {
+            agents: [],
+            role: [],
+            abilities: []
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+    
+    },
+    computed: {
+        
+    }
+}).mount('#app')
