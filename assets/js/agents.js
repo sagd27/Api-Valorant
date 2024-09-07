b0b3cbca4168eab8602c5b63bb2220a0ce54bbda
Jlorduyz <jorgelorduyeli4@outlook.com>
2024-09-06 19:04:22 -0500

    agents realizada

---
+++ assets/js/agents.js
@@ -0,0 +1,77 @@
+let urlValo = "https://valorant-api.com/v1/agents";
+
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      agentes: [],
+      agentesBK: [],
+      roles: [],
+      rolesSelecionados: [],
+      textoBusqueda: "",
+      agentesFavoritos: [],
+    };
+  },
+  created() {
+    this.traerData(urlValo);
+    let localData = JSON.parse(localStorage.getItem("agentesFavoritos"));
+    if (localData) {
+      this.agentesFavoritos = localData;
+    }
+  },
+  methods: {
+    traerData(url) {
+      fetch(url)
+        .then((response) => response.json())
+        .then((res) => {
+          this.agentes = res.data.filter(
+            (agente) => agente.isPlayableCharacter === true
+          );
+          this.agentesBK = this.agentes;
+
+          this.roles = this.roles = Array.from(
+            new Set(this.agentes.map((res) => res.role.displayName))
+          );
+        });
+    },
+    agregarFavorito(agente) {
+      if (!this.agentesFavoritos.includes(agente)) {
+        this.agentesFavoritos.push(agente);
+        localStorage.setItem(
+          "agentesFavoritos",
+          JSON.stringify(this.agentesFavoritos)
+        );
+      }
+    },
+    eliminarFavorito(agente) {
+      let index = this.agentesFavoritos.findIndex(
+        (res) => res.uuid == agente.uuid
+      );
+      if (index !== -1) {
+        this.agentesFavoritos.splice(index, 1);
+        localStorage.setItem(
+          "agentesFavoritos",
+          JSON.stringify(this.agentesFavoritos)
+        );
+      }
+    },
+  },
+  computed: {
+    filtros() {
+      let filtroA = this.agentesBK.filter((agente) =>
+        agente.displayName
+          .toLowerCase()
+          .includes(this.textoBusqueda.toLowerCase())
+      );
+      this.agentes = filtroA;
+      if (this.rolesSelecionados.length > 0) {
+        this.agentes = filtroA.filter((res) =>
+          this.rolesSelecionados.includes(res.role.displayName)
+        );
+      } else {
+        this.agentes = filtroA;
+      }
+    },
+  },
+}).mount("#app");
