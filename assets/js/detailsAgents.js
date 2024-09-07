81adf1d54e6fed5a87f56f1faf570c81d8c741cc
Juan Bocanegra <jbocanegrae@unal.edu.co>
2024-09-07 13:06:46 -0500

    creadas las cards de detalles para agents.html

--- assets/js/detailsAgents.js
+++ assets/js/detailsAgents.js
@@ -5,15 +5,35 @@ const {createApp} = Vue
 const app = createApp({
     data() {
         return {
-            agents: [],
-            role: [],
-            abilities: []
+            agents: null,
+            popoverIndex: null
         }
     },
     created() {
+        const URLParams = new URLSearchParams(window.location.search);
+        const id = URLParams.get('uuid'); 
+        console.log(id);
+        
+        if (id) {
+            
+            fetch(url + '/' + id)
+            .then(response => response.json())
+            .then(data => {
+                this.agents = data.data;       
+                console.log(this.agents);
+                
+            })
+            .catch(error => console.log('Error fetching agent details:' , error))
+        }
         
     },
     methods: {
+        showPopover(index, description) {
+            this.popoverIndex = index;
+        },
+        hidePopover() {
+            this.popoverIndex = null;
+        }
     
     },
     computed: {
